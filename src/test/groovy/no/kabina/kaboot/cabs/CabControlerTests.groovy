9dcb9fd33b62105be64139a8b4a8cbc1a9d57998
Bogusz Jelinski <Bogusz.Jelinski@skatteetaten.no>
2020-10-22 15:34:23 +0200

    spock cont.

--- src/test/groovy/no/kabina/kaboot/cabs/CabControlerTests.groovy
+++ src/test/groovy/no/kabina/kaboot/cabs/CabControlerTests.groovy
@@ -3,23 +3,33 @@ package no.kabina.kaboot.cabs
 import org.junit.Ignore
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.http.HttpStatus.*
+
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.test.web.servlet.MockMvc
+
+import no.kabina.kaboot.orders.TaxiOrderRepository
+import no.kabina.kaboot.orders.TaxiOrderService
+import no.kabina.kaboot.routes.RouteRepository
+import no.kabina.kaboot.routes.TaskRepository
 import spock.lang.Specification
 import spock.mock.DetachedMockFactory
 
+@AutoConfigureMockMvc
 @WebMvcTest
 class CabControlerTests extends Specification {
     def cabController = new CabController()
-    MockMvc mockMvc = standaloneSetup(searchController).build()
+    MockMvc mockMvc = standaloneSetup(cabController).build()
+    def cabService = Mock(CabRepository)
 
-    @SpringBean
-    CabRepository cabService
+   /* @Autowired
+    CabRepository cabRepository*/
 
     def setup() {
         cabController.repository = cabService
@@ -36,7 +46,7 @@ class CabControlerTests extends Specification {
 
         then: "Status is 200 and the response is 'Hello world!'"
         response.status == OK.value()
-        response.contentAsString == "[]"
+        response.contentAsString == "" // "{\"id\":0,\"location\":0,\"status\":\"ASSIGNED\"}"
     }
 
     @TestConfiguration
@@ -47,5 +57,26 @@ class CabControlerTests extends Specification {
         CabRepository cabService() {
             return detachedMockFactory.Stub(CabRepository)
         }
+
+        @Bean
+        TaxiOrderRepository taxiOrderRepository() {
+          return detachedMockFactory.Stub(TaxiOrderRepository)
+        }
+
+        @Bean
+        TaxiOrderService taxiOrderService() {
+          return detachedMockFactory.Stub(TaxiOrderService)
+        }
+
+        @Bean
+        RouteRepository routeRepository() {
+          return detachedMockFactory.Stub(RouteRepository)
+        }
+
+        @Bean
+        TaskRepository taskRepository() {
+          return detachedMockFactory.Stub(TaskRepository)
+        }
+
     }
 }
