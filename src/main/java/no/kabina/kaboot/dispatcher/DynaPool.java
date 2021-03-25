cd1daee4d52ce079db2676fc0583468f8491446f
Bogusz Jelinski <bogusz.jelinski@gmail.com>
2021-03-25 14:13:00 +0100

    DynaPool draft #3

--- src/main/java/no/kabina/kaboot/dispatcher/DynaPool.java
+++ src/main/java/no/kabina/kaboot/dispatcher/DynaPool.java
@@ -1,18 +1,21 @@
 import static java.lang.StrictMath.abs;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.HashMap;
-import java.util.Map.Entry;
+//import java.util.Map.Entry;
 import java.util.ArrayList;
 import java.util.List;
 
 // recursion deep down, n=500, 54s, Count: 348496095
 // dyna: Count n=500, count=(25825, 712277, 14161374), 8s
+// no duplicates: Count n=500, count=(25825, 513849, 4467560), 19s
+
 public class DynaPool {
-    final int MAX_CUST = 500;
+    final int MAX_CUST = 600;
     final int MAX_LEV = 4;
     final int MAX_TRIP = 4;
     final int MAX_STAND = 50;
-    final double MAX_LOSS = 1.3;
+    final double MAX_LOSS = 1.1;
     int count=0;
 
     int[] from = new int[MAX_CUST];
@@ -27,6 +30,7 @@ public class DynaPool {
         p.storeLeaves();
         p.dive(0);
         p.raport();
+        //p.removeDuplicates();
     }
 
     private void raport() {
@@ -45,14 +49,21 @@ public class DynaPool {
         if (lev > MAX_LEV-3) return; // last two levels are "leaves"
 
         dive(lev + 1);
-
+        long time1 = System.currentTimeMillis();
+        
         for (int c = 0; c < MAX_CUST; c++) {
-            for(Branch b : node[lev+1]) { // we iterate over product of the stage further in the tree: +1
+            for (Branch b : node[lev+1]) { // we iterate over product of the stage further in the tree: +1
                 if (!isFoundInBranchOrTooLong(c, b)) { // one of two in 'b' is dropped off earlier
                     storeBranch(lev, c, b);
                 }
             }
         }
+        // removing duplicates which come from lev+1
+        long time2 = System.currentTimeMillis();
+        System.out.println("Finding branches: " + (time2-time1)/1000);
+        rmDuplicates(lev);
+        long time3 = System.currentTimeMillis();
+        System.out.println("Rm duplicates: " + (time3-time2)/1000);
     }
 
     private void storeBranch(int lev, int c, Branch b) {
@@ -61,12 +72,13 @@ public class DynaPool {
         for(int j=0; j<MAX_LEV-lev-1; j++) // further stage has one passenger less: -1
             drops[j+1] = b.dropoff[j];
         
-        Branch b2 = new Branch(cost(to[c], to[b.dropoff[0]]) + b.cost, drops);
+        Branch b2 = new Branch(c + "-" + b.key, // no sorting as we have to remove lev+1 duplicates eg. 1-4-5 and 1-5-4
+                               cost(to[c], to[b.dropoff[0]]) + b.cost, 
+                               drops);
         node[lev].add(b2);
     }
 
     private void storeLeaves() {
-     
         for (int c = 0; c < MAX_CUST; c++) 
             for (int d = 0; d < MAX_CUST; d++) 
                 if (c != d && 
@@ -74,7 +86,8 @@ public class DynaPool {
                         int[] drops = new int[2];
                         drops[0] = c;
                         drops[1] = d;
-                        Branch b = new Branch(cost(to[c], to[d]), drops);
+                        String key = c>d? d+"-"+c : c+"-"+d;
+                        Branch b = new Branch(key, cost(to[c], to[d]), drops);
                         node[MAX_LEV-2].add(b);
         }
     }
@@ -90,6 +103,103 @@ public class DynaPool {
                 return true;
             if (i+1 < b.dropoff.length)
                 wait += cost(to[b.dropoff[i]], to[b.dropoff[i+1]]); 
+        }
+        return false;
+    }
+
+    private void rmDuplicates(int lev) {
+        Branch[] arr = node[lev].toArray(new Branch[0]);
+        if (arr == null || arr.length<2) return;
+
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i].cost == -1) { // this -1 marker is set below
+                continue;
+            }
+            if (arr[i].key.equals(arr[i+1].key)) {
+                if (arr[i].cost > arr[i+1].cost)
+                    arr[i].cost = -1; // to be deleted
+                else arr[i+1].cost = -1;
+            }
+        }
+        // removing but also recreating the key - must be sorted
+        
+        List<Branch> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) 
+            if (arr[i].cost != -1) {
+                int[] copiedArray = Arrays.copyOf(arr[i].dropoff, arr[i].dropoff.length);
+                Arrays.sort(copiedArray);
+                String key="";
+                for (int j = 0; j < copiedArray.length; j++) {
+                    key += copiedArray[j] + j<copiedArray.length-1 ? "-" : "";
+                }
+                arr[i].key = key;
+                list.add(arr[i]);
+            }
+        
+        node[lev] = list;
+    }
+
+   /* private boolean isSame(Branch a, Branch b) {
+        for (int i=0; i < a.dropoff.length; i++) 
+          if (a.dropoff[i] != b.dropoff[i])
+            return false;
+        return true;
+    }
+*/
+    public HashMap<String, Branch> removeDuplicates() {
+        int inPool = MAX_LEV;
+        long time1 = System.currentTimeMillis();
+        Branch[] arr = node[0].toArray(new Branch[0]);
+
+        if (arr == null) {
+          return null;
+        }
+        Arrays.sort(arr);
+        long time2 = System.currentTimeMillis();
+        System.out.println("Sorting took: " + (time2-time1)/1000);
+        // removing duplicates
+        int i = 0, j = 0;
+        for (i = 0; i < arr.length; i++) {
+          if (arr[i].cost == -1) { // this -1 marker is set below
+            continue;
+          }
+          for (j = i + 1; j < arr.length; j++) {
+            if (arr[j].cost != -1 // not invalidated; this check is for performance reasons
+                    && isFound(arr, i, j)) {
+              arr[j].cost = -1; // duplicated; we remove an element with greater costs (list is pre-sorted)
+            }
+          }
+        }
+        long time3 = System.currentTimeMillis();
+        System.out.println("Rm duplicates took: " + (time3-time2)/1000);
+        // just collect non-duplicated pool plans
+        HashMap<String, Branch> ret = new HashMap<>();
+        String key= null;
+        for (i = 0; i < arr.length; i++) {
+          if (arr[i].cost != -1) {
+            Arrays.sort(arr[i].dropoff);
+            key="";
+            //for (j=0; j<arr[i].dropoff.length; j++)
+            //    key += arr[i].dropoff[j] + "-";
+            key = arr[i].dropoff[0] + "-" + arr[i].dropoff[1] + "-" 
+                    + arr[i].dropoff[2] + "-" + arr[i].dropoff[3];
+            ret.put(key, arr[i]);
+          }
+        }
+        long time4 = System.currentTimeMillis();
+        System.out.println("Creating hash: " + (time4-time3)/1000);
+        return ret;
+    }
+
+    public boolean isFound(Branch[] arr, int i, int j) {
+        for (int x = 0; x < arr[j].dropoff.length; x++) { 
+          for (int y = 0; y < arr[i].dropoff.length; y++) {
+            if (arr[j].dropoff[x] == arr[i].dropoff[y]) {
+              return true;
+            }
+          }
         }
         return false;
     }
@@ -116,25 +226,26 @@ public class DynaPool {
     }
 
     class Branch implements Comparable<Branch> {
+        public String key; // used to remove duplicates and search in hashmap
         public int cost;
         public int[] dropoff; // we could get rid of it to gain on memory (key stores this too); but we would lose time on parsing 
 
-        Branch (int cost, int[] drops){
+        Branch (String key, int cost, int[] drops){
+            this.key = key;
             this.cost = cost;
             this.dropoff = drops;
         }
 
         @Override
         public int compareTo(Branch pool) {
-          return this.cost - pool.cost;
-        }
+          return this.key.compareTo(pool.key);        }
       
         @Override
         public boolean equals(Object pool) {
           if (pool == null || this.getClass() != pool.getClass()) {
             return false;
           }
-          return this.cost == ((Branch) pool).cost;
+          return this.key.equals(((Branch) pool).key);
         }
       
         @Override
