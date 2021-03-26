0ac187be2a7fc9fb43ea0149c264ce8fb87c4891
Bogusz Jelinski <bogusz.jelinski@gmail.com>
2021-03-26 11:04:31 +0100

    DynaPool draft #3

--- src/main/java/no/kabina/kaboot/dispatcher/DynaPool.java
+++ src/main/java/no/kabina/kaboot/dispatcher/DynaPool.java
@@ -2,7 +2,6 @@ import static java.lang.StrictMath.abs;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.HashMap;
-//import java.util.Map.Entry;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,45 +10,72 @@ import java.util.List;
 // no duplicates: Count n=500, count=(25825, 513849, 4467560), 19s
 
 public class DynaPool {
-    final int MAX_CUST = 600;
+    final int MAX_CUST = 200;
     final int MAX_LEV = 4;
     final int MAX_TRIP = 4;
     final int MAX_STAND = 50;
-    final double MAX_LOSS = 1.1;
+    final double MAX_LOSS = 1.2;
     int count=0;
 
     int[] from = new int[MAX_CUST];
     int[] to = new int[MAX_CUST];
     Random rand = new Random(10L);
-    List<Branch>[] node = /*(ArrayList<Branch>[])*/ new ArrayList[MAX_LEV-1];
+    List<Branch>[] node = new ArrayList[MAX_LEV-1];
+    List<Branch>[] nodeP = new ArrayList[MAX_LEV-1];
+    HashMap<String, Branch> map = new HashMap<>();
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         DynaPool p = new DynaPool();
         p.initMem();
         p.genDemand();
         p.storeLeaves();
+        p.storePickUpLeaves();
         p.dive(0);
+        p.diveP(0);
+        long time1 = System.currentTimeMillis();
         p.raport();
-        //p.removeDuplicates();
+        p.raportP();
+        
+        List<Pool> ret = p.mergeTrees();
+        
+        long time2 = System.currentTimeMillis();
+        System.out.println("Time of merge: " + (time2-time1)/1000 + "s");
+        System.out.println("Size after merge: " + ret.size());
+
+        ret = p.removeDuplicates(ret);
+        
+        long time3 = System.currentTimeMillis();
+        System.out.println("Time of rm duplicates: " + (time3-time2)/1000 + "s");
+        System.out.println("Total time: " + (time3-start)/1000 + "s");
+        System.out.println("Size after removing duplicates: " + ret.size());
     }
 
     private void raport() {
         System.out.println("Count leaves: " + node[MAX_LEV-2].size());
         System.out.println("Count between: " + node[MAX_LEV-3].size());
-        System.out.println("Count root: " + node[MAX_LEV-4].size());
+        System.out.println("Count root: " + map.size());
+        //System.out.println("Number of elements: " + myMap2.size());
+    }
+
+    private void raportP() {
+        System.out.println("Count leaves: " + nodeP[MAX_LEV-2].size());
+        System.out.println("Count between: " + nodeP[MAX_LEV-3].size());
+        System.out.println("Count root: " + nodeP[MAX_LEV-4].size());
         //System.out.println("Number of elements: " + myMap2.size());
     }
 
     private void initMem() {
-        for (int i=0; i<MAX_LEV-1; i++) 
+        for (int i=0; i<MAX_LEV-1; i++) {
             node[i] = new ArrayList<>(); 
+            nodeP[i] = new ArrayList<>(); 
+        }
     }
 
     public void dive(int lev) {
         if (lev > MAX_LEV-3) return; // last two levels are "leaves"
 
         dive(lev + 1);
-        long time1 = System.currentTimeMillis();
         
         for (int c = 0; c < MAX_CUST; c++) {
             for (Branch b : node[lev+1]) { // we iterate over product of the stage further in the tree: +1
@@ -59,11 +85,7 @@ public class DynaPool {
             }
         }
         // removing duplicates which come from lev+1
-        long time2 = System.currentTimeMillis();
-        System.out.println("Finding branches: " + (time2-time1)/1000);
-        rmDuplicates(lev);
-        long time3 = System.currentTimeMillis();
-        System.out.println("Rm duplicates: " + (time3-time2)/1000);
+        node[lev] = rmDuplicates(node[lev], lev, true); // true - make hash
     }
 
     private void storeBranch(int lev, int c, Branch b) {
@@ -92,6 +114,48 @@ public class DynaPool {
         }
     }
 
+    private void storePickUpLeaves() {
+        for (int c = 0; c < MAX_CUST; c++) 
+            for (int d = 0; d < MAX_CUST; d++) 
+                if (c != d && 
+                    cost(from[c], from[d]) < cost(from[d], to[d]) * MAX_LOSS) {
+                        int[] pickup = new int[2];
+                        pickup[0] = c;
+                        pickup[1] = d;
+                        String key = c>d? d+"-"+c : c+"-"+d;
+                        Branch b = new Branch(key, cost(from[c], from[d]), pickup);
+                        nodeP[MAX_LEV-2].add(b);
+        }
+    }
+
+    public void diveP(int lev) {
+        if (lev > MAX_LEV-3) return; // last two levels are "leaves"
+
+        diveP(lev + 1);
+        
+        for (int c = 0; c < MAX_CUST; c++) {
+            for (Branch b : nodeP[lev+1]) { // we iterate over product of the stage further in the tree: +1
+                if (!isFoundInBranchOrTooLongP(c, b)) { // one of two in 'b' is dropped off earlier
+                    storeBranchP(lev, c, b);
+                }
+            }
+        }
+        // removing duplicates which come from lev+1
+        nodeP[lev] = rmDuplicates(nodeP[lev], lev, false);
+    }
+
+    private void storeBranchP(int lev, int c, Branch b) {
+        int[] pickups = new int[MAX_LEV-lev];
+        pickups[0] = c;
+        for(int j=0; j<MAX_LEV-lev-1; j++) // further stage has one passenger less: -1
+            pickups[j+1] = b.dropoff[j];
+        
+        Branch b2 = new Branch(c + "-" + b.key, // no sorting as we have to remove lev+1 duplicates eg. 1-4-5 and 1-5-4
+                               cost(from[c], from[b.dropoff[0]]) + b.cost, 
+                               pickups);
+        nodeP[lev].add(b2);
+    }
+
     public boolean isFoundInBranchOrTooLong(int c, Branch b) {
         for (int i=0; i<b.dropoff.length; i++)
             if (b.dropoff[i] == c) return true; // current passenger is in the branch below -> reject that combination
@@ -107,9 +171,24 @@ public class DynaPool {
         return false;
     }
 
-    private void rmDuplicates(int lev) {
-        Branch[] arr = node[lev].toArray(new Branch[0]);
-        if (arr == null || arr.length<2) return;
+    public boolean isFoundInBranchOrTooLongP(int c, Branch b) {
+        for (int i=0; i<b.dropoff.length; i++)
+            if (b.dropoff[i] == c) return true; // current passenger is in the branch below -> reject that combination
+        // now checking if anyone in the branch does not lose too much with the pool
+        int wait = cost(from[c], from[b.dropoff[0]]);
+        
+        for (int i=0; i<b.dropoff.length; i++) {
+            if (wait > cost(from[b.dropoff[i]], to[b.dropoff[i]]) * MAX_LOSS ) 
+                return true;
+            if (i+1 < b.dropoff.length)
+                wait += cost(from[b.dropoff[i]], from[b.dropoff[i+1]]); 
+        }
+        return false;
+    }
+
+    private List<Branch> rmDuplicates(List<Branch> node, int lev, boolean makeHash) {
+        Branch[] arr = node.toArray(new Branch[0]);
+        if (arr == null || arr.length<2) return null;
 
         Arrays.sort(arr);
         
@@ -126,39 +205,63 @@ public class DynaPool {
         // removing but also recreating the key - must be sorted
         
         List<Branch> list = new ArrayList<>();
+        
         for (int i = 0; i < arr.length; i++) 
             if (arr[i].cost != -1) {
                 int[] copiedArray = Arrays.copyOf(arr[i].dropoff, arr[i].dropoff.length);
                 Arrays.sort(copiedArray);
                 String key="";
                 for (int j = 0; j < copiedArray.length; j++) {
-                    key += copiedArray[j] + j<copiedArray.length-1 ? "-" : "";
+                    key += copiedArray[j];
+                    if (j < copiedArray.length-1) key += "-";
                 }
                 arr[i].key = key;
-                list.add(arr[i]);
+                if (lev == 0 && makeHash) map.put(key, arr[i]);
+                else list.add(arr[i]);
             }
-        
-        node[lev] = list;
+        return list;
     }
 
-   /* private boolean isSame(Branch a, Branch b) {
-        for (int i=0; i < a.dropoff.length; i++) 
-          if (a.dropoff[i] != b.dropoff[i])
-            return false;
-        return true;
+    private List<Pool> mergeTrees() {
+        List<Pool> ret = new ArrayList<>();
+
+        for (Branch p : nodeP[0]) {
+            Branch d = map.get(p.key); // get drop-offs for that key
+            if (d==null) continue; // drop-off was not acceptable
+            // checking if drop-off still acceptable with that pick-up fase
+            int wait = p.cost + cost(p.dropoff[p.dropoff.length-1], d.dropoff[0]);
+            int i=0;
+            boolean tooLong= false;
+            while(true) {
+                if (wait > cost(from[d.dropoff[i]], to[d.dropoff[i]]) * MAX_LOSS) {
+                    tooLong = true;
+                    break;
+                }
+                if (i == d.dropoff.length -1) break;
+                wait += cost(to[d.dropoff[i]], to[d.dropoff[i+1]]);
+                i++;
+            }
+            if (tooLong) continue;
+            // a viable plan -> merge pickup and dropoff
+            int[] both = new int[p.dropoff.length + d.dropoff.length];
+            i=0;
+            for (; i<p.dropoff.length; i++) both[i] = p.dropoff[i];
+            for (; i<p.dropoff.length+d.dropoff.length; i++) both[i] = d.dropoff[i-p.dropoff.length];
+            int cost = p.cost + cost(p.dropoff[p.dropoff.length-1], d.dropoff[0]) + d.cost;
+            ret.add(new Pool(cost, both));
+        }
+        return ret;
     }
-*/
-    public HashMap<String, Branch> removeDuplicates() {
+
+    public List<Pool> removeDuplicates(List<Pool> list) {
         int inPool = MAX_LEV;
-        long time1 = System.currentTimeMillis();
-        Branch[] arr = node[0].toArray(new Branch[0]);
+        Pool[] arr = list.toArray(new Pool[0]);
 
         if (arr == null) {
           return null;
         }
         Arrays.sort(arr);
-        long time2 = System.currentTimeMillis();
-        System.out.println("Sorting took: " + (time2-time1)/1000);
+
         // removing duplicates
         int i = 0, j = 0;
         for (i = 0; i < arr.length; i++) {
@@ -172,31 +275,22 @@ public class DynaPool {
             }
           }
         }
-        long time3 = System.currentTimeMillis();
-        System.out.println("Rm duplicates took: " + (time3-time2)/1000);
+        
         // just collect non-duplicated pool plans
-        HashMap<String, Branch> ret = new HashMap<>();
-        String key= null;
+        List<Pool> ret = new ArrayList<>();
+
         for (i = 0; i < arr.length; i++) {
           if (arr[i].cost != -1) {
-            Arrays.sort(arr[i].dropoff);
-            key="";
-            //for (j=0; j<arr[i].dropoff.length; j++)
-            //    key += arr[i].dropoff[j] + "-";
-            key = arr[i].dropoff[0] + "-" + arr[i].dropoff[1] + "-" 
-                    + arr[i].dropoff[2] + "-" + arr[i].dropoff[3];
-            ret.put(key, arr[i]);
+            ret.add(arr[i]);
           }
         }
-        long time4 = System.currentTimeMillis();
-        System.out.println("Creating hash: " + (time4-time3)/1000);
         return ret;
     }
 
-    public boolean isFound(Branch[] arr, int i, int j) {
-        for (int x = 0; x < arr[j].dropoff.length; x++) { 
-          for (int y = 0; y < arr[i].dropoff.length; y++) {
-            if (arr[j].dropoff[x] == arr[i].dropoff[y]) {
+    public boolean isFound(Pool[] arr, int i, int j) {
+        for (int x = 0; x < arr[j].stops.length; x++) { 
+          for (int y = 0; y < arr[i].stops.length; y++) {
+            if (arr[j].stops[x] == arr[i].stops[y]) {
               return true;
             }
           }
@@ -253,6 +347,37 @@ public class DynaPool {
             int result = (int) (dropoff[0] ^ (dropoff[0] >>> 32));
             result = 31 * result + cost;
             result = 31 * result + dropoff[1];
+            return result;
+        }
+      
+    }
+    class Pool implements Comparable<Pool> {
+        public int cost;
+        public int[] stops; // we could get rid of it to gain on memory (key stores this too); but we would lose time on parsing 
+
+        Pool (int cost, int[] stops){
+            this.cost = cost;
+            this.stops = stops;
+        }
+
+        @Override
+        public int compareTo(Pool pool) {
+          return this.cost - pool.cost;
+        }
+      
+        @Override
+        public boolean equals(Object pool) {
+          if (pool == null || this.getClass() != pool.getClass()) {
+            return false;
+          }
+          return this.cost == ((Pool) pool).cost;
+        }
+      
+        @Override
+        public int hashCode() {
+            int result = (int) (stops[0] ^ (stops[0] >>> 32));
+            result = 31 * result + cost;
+            result = 31 * result + stops[1];
             return result;
         }
       
